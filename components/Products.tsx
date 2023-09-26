854d0f32a4954ef811fb2066fd7e002dc0e5e880
Philipp Isaak <phi.isaak@gmail.com>
2023-09-26 22:06:18 +0200

    fixes after merge

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,5 +1,6 @@
 "use client";
 import  useProducts  from '@/hooks/useProducts';
+import Image from "next/image";
 
 export function Products() {
   const {products, isLoading, isError} = useProducts();
@@ -7,26 +8,28 @@ export function Products() {
   if (isError) return <p>Error: {isError.message}</p>;
   
   return (
-    <div>
-      <div className="border">
-        <h1>Products</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
         {/* <p>{JSON.stringify(products)}</p> */}
-        <div>
           {products!.map((product) => (
-            <ul key={product.id}>
-              <li>{product.id}</li>
-              <li>{product.name}</li>
-              <li>{product.description}</li>
-              <li>{product.price}</li>
-              <li>{product.inStock}</li>
-              <li>{product.createdAt}</li>
-              <li>{product.updatedAt}</li>
-            </ul>
+            <div key={product.id} className="card border shadow-xl rounded-xl hover:scale-101 hover:shadow-2xl transition">
+              <div>
+                <Image
+                    height="1000"
+                    width="1000"
+                    src="/shirt-player.png"
+                    alt="Shirt"
+                    className="rounded-t-xl"
+                />
+              </div>
+              <div className="p-5">
+                <h2 className="font-bold">{product.name}</h2>
+                <p className="mb-5">{product.description}</p>
+                <hr/>
+                <p className="font-bold mt-5">{product.price}</p>
+              </div>
+            </div>
           ))}
-        </div>
-
       </div>
-    </div>
   );
 }
 
