62d5cf36369acf8df5f73efafce43413c83bb72c
Roger Killer <roger.killer@advery.ch>
2023-10-13 15:04:25 +0200

    ✨ feat(page.tsx): add Dashboard component to render a form and products section on the dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Form } from "../components/Form";
+import { Products } from "../components/Product";
+import { Section } from "../components/Base/Section";
+import { Container } from "../components/Base/Container";
+
+export default function Dashboard() {
+  return (
+    <Section>
+      <Container>
+        <Form />
+        <Products />
+      </Container>
+    </Section>
+  );
+}
