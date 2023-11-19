2962aede2115ef54269c3ba9cd705cbb5036845a
Roger Killer <roger.killer@advery.ch>
2023-11-19 16:06:20 +0100

    ✨ feat(Register): add Register page component
    A new file `page.tsx` was added to the `Register` directory. This file contains the `Register` page component, which renders a section with a container and a heading "register".

---
+++ app/Register/page.tsx
@@ -0,0 +1,12 @@
+import { Section } from "../components/Base/Section";
+import { Container } from "../components/Base/Container";
+
+export default function Register() {
+  return (
+    <Section>
+      <Container>
+        <h1>register</h1>
+      </Container>
+    </Section>
+  );
+}
