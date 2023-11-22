1f55c09d984ad2abd94e62e4b8bdae94d1c3eed3
Roger Killer <roger.killer@advery.ch>
2023-11-22 16:34:27 +0100

    create register page

--- app/Register/page.tsx
+++ app/Register/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { Input } from "@nextui-org/react";
 import { Section } from "../components/Base/Section";
 import { Container } from "../components/Base/Container";
 
@@ -6,6 +8,48 @@ export default function Register() {
     <Section>
       <Container>
         <h1>register</h1>
+        <form action="">
+          <div className="my-2">
+            <Input
+              type="text"
+              variant={"bordered"}
+              label="Name"
+              name="name"
+            />
+          </div>
+          <div className="my-2">
+            <Input
+              type="text"
+              variant={"bordered"}
+              label="Vorname"
+              name="vorname"
+            />
+          </div>
+          <div className="my-2">
+            <Input
+              type="text"
+              variant={"bordered"}
+              label="Email"
+              name="email"
+            />
+          </div>
+          <div className="my-2">
+            <Input
+              type="password"
+              variant={"bordered"}
+              label="Passwort"
+              name="password"
+            />
+          </div>
+          <div className="my-2">
+            <Input
+              type="password"
+              variant={"bordered"}
+              label="Passwort wiederholen"
+              name="password"
+            />
+            </div>
+        </form>
       </Container>
     </Section>
   );
