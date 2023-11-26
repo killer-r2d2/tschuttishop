c7c3807c967d346fc43bb8121b971a746ce83824
Roger Killer <roger.killer@advery.ch>
2023-11-26 19:10:59 +0100

    reorganize RootLayout

---
+++ app/Login/page.tsx
@@ -0,0 +1,6 @@
+export default function LoginPage() {
+
+  return (
+    <h1>Login Page</h1>
+  )
+}
--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import { NextUIProvider } from "@nextui-org/react";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <div className="flex flex-col min-h-screen">{children}</div>
+      {children}
     </NextUIProvider>
   );
 }
