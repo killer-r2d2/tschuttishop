c37141ebabe2f6106796ecc9451c19ca4c02d070
Roger Killer <roger.killer@advery.ch>
2023-12-03 19:29:39 +0100

    create AuthButton

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AuthButton from "./components/AuthButton";
 import { Form } from "./components/Form";
 import { Hero } from "./components/Hero";
 import { ProductList } from "./components/ProductsList";
@@ -5,6 +6,7 @@ import { ProductList } from "./components/ProductsList";
 export default async function Home() {
   return (
     <div>
+      <AuthButton />
       <Hero />
       <ProductList />
     </div>
