c4775a42082eec5650c93bf0de7b55a2d2f51c6c
Roger Killer <roger.killer@advery.ch>
2023-12-09 14:40:01 +0100

    change login to Login in link. Add redirect

--- app/DashboardProduct/page.tsx
+++ app/DashboardProduct/page.tsx
@@ -2,8 +2,20 @@ import { Form } from "../components/Form";
 import { Products } from "../components/Product";
 import { Section } from "../components/Base/Section";
 import { Container } from "../components/Base/Container";
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    redirect("/Login");
+  }
   return (
     <Section>
       <Container>
