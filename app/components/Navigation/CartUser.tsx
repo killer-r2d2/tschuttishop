a7d38041c371b0792230da8b987a20d59892a0e6
Roger Killer <roger.killer@advery.ch>
2023-12-10 09:18:58 +0100

    remove user Icon in navigation

--- app/components/Navigation/CartUser.tsx
+++ app/components/Navigation/CartUser.tsx
@@ -4,9 +4,6 @@ import { ShoppingCartIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 export function CartUser() {
   return (
     <div className="flex gap-4">
-      <Link href="/DashboardProduct">
-        <UserCircleIcon className="w-8 text-slate-100 hover:text-slate-500 transition-colors" />
-      </Link>
       <Link href="/Cart">
         <ShoppingCartIcon className="w-8 text-slate-100 hover:text-slate-500 transition-colors" />
       </Link>
