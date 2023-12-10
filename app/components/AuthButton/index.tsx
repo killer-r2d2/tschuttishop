5e13e9ddf43bfa973fe1be439f85e0c08a6f9f89
Roger Killer <roger.killer@advery.ch>
2023-12-10 13:02:00 +0100

    add the AuthButton to layout

--- app/components/AuthButton/index.tsx
+++ app/components/AuthButton/index.tsx
@@ -1,52 +1,51 @@
-import { createClient } from '@/utils/supabase/server'
-import Link from 'next/link'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-import { Section } from '../Base/Section'
-import { Container } from '../Base/Container'
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { Container } from "../Base/Container";
 
 export default async function AuthButton() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
 
   const {
     data: { user },
-  } = await supabase.auth.getUser()
+  } = await supabase.auth.getUser();
 
   const signOut = async () => {
-    'use server'
+    "use server";
 
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-    await supabase.auth.signOut()
-    return redirect('/Login')
-  }
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.signOut();
+    return redirect("/Login");
+  };
 
   return user ? (
-      <Container>
-    <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      </form>
+    <div className="container mx-auto ps-5 pe-5 mb-5">
+      <div className="flex items-center gap-4">
+        Hey, {user.email}!
+        <form action={signOut}>
+          <button className="py-2 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+            Logout
+          </button>
+        </form>
+        <Link
+          href="/DashboardProduct"
+          className="py-2 rounded-md max-w-max no-underline bg-btn-background hover:bg-btn-background-hover"
+        >
+          Dashboard
+        </Link>
+      </div>
+    </div>
+  ) : (
+    <div className="container mx-auto ps-5 pe-5 mb-5">
       <Link
-        href="/DashboardProduct"
-        className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+        href="/Login"
+        className="py-2 flex rounded-md max-w-max no-underline bg-btn-background hover:bg-btn-background-hover"
       >
-        Dashboard
+        Login
       </Link>
     </div>
-      </Container>
-  ) : (
-    <Container>
-    <Link
-      href="/Login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
-    >
-      Login
-    </Link>
-    </Container>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
-import AuthButton from "./components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 import { Hero } from "./components/Hero";
 import { ProductList } from "./components/ProductsList";
 
 export default async function Index() {
-  const cookieStore = cookies();
-
-  const canInitSupabaseClient = () => {
-    try {
-      createClient(cookieStore);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div>
-      {isSupabaseConnected && <AuthButton />}
       <Hero />
       <ProductList />
     </div>
