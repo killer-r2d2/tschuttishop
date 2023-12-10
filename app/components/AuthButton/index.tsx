b8662045e7c7157c939eaa80feb2446ee32bef2e
Roger Killer <roger.killer@advery.ch>
2023-12-10 09:18:29 +0100

    add link to DashboardProduct in AuthButton component

--- app/components/AuthButton/index.tsx
+++ app/components/AuthButton/index.tsx
@@ -2,6 +2,8 @@ import { createClient } from '@/utils/supabase/server'
 import Link from 'next/link'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { Section } from '../Base/Section'
+import { Container } from '../Base/Container'
 
 export default async function AuthButton() {
   const cookieStore = cookies()
@@ -21,6 +23,7 @@ export default async function AuthButton() {
   }
 
   return user ? (
+      <Container>
     <div className="flex items-center gap-4">
       Hey, {user.email}!
       <form action={signOut}>
@@ -28,13 +31,22 @@ export default async function AuthButton() {
           Logout
         </button>
       </form>
+      <Link
+        href="/DashboardProduct"
+        className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      >
+        Dashboard
+      </Link>
     </div>
+      </Container>
   ) : (
+    <Container>
     <Link
       href="/Login"
       className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
     >
       Login
     </Link>
+    </Container>
   )
 }
