95c3062faaa7cb5761d9334f69a3494bebcb339a
Roger Killer <roger.killer@advery.ch>
2023-12-30 10:26:26 +0100

    changed items items: number[] | []; to items: number[]; in store/cartState.ts

--- store/cartState.ts
+++ store/cartState.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 interface CartState {
-  items: number[] | [];
+  items: number[];
   count: number;
   addItem: (id: number) => void;
   deleteItem: (id: number) => void;
