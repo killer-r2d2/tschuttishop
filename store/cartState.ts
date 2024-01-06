57c409399d781efd679e2b75c4f5fdf40f13b25b
Philipp Isaak <phi.isaak@gmail.com>
2024-01-06 14:59:46 +0100

    write the cartState to localstorage with a useEffect because parsing with zustand doesn't work with next serverside stuff

--- store/cartState.ts
+++ store/cartState.ts
@@ -8,23 +8,39 @@ interface CartState {
   clearCart: () => void;
 }
 
-export let cartStore = create<CartState>()((set) => ({
-  items: [],
-  count: 0,
-  addItem: (id: number) => {
-    if (cartStore.getState().items.includes(id)) {
-      alert("Das Produkt ist bereits im Warenkorb!");
-    } else {
-      set((state) => ({ items: [...state.items, id] }));
-      set((state) => ({ count: state.count + 1 }));
-    }
-  },
-  deleteItem: (id: number) => {
-    set((state) => ({ items: state.items.filter((item) => item !== id) }));
-    set((state) => ({ count: state.count - 1 }));
-  },
-  clearCart: () => {
-    set((state) => ({ items: [] }));
-    set((state) => ({ count: 0 }));
-  }
-}));
+const localStorageKey: string = "cartState";
+
+export let cartStore = create<CartState>()((set) => {
+  // Load state from localStorage when the store is initialized
+  const storedState = localStorage.getItem(localStorageKey);
+  const initialState: CartState = storedState
+    ? JSON.parse(storedState)
+    : { items: [], count: 0 };
+
+  return {
+    ...initialState,
+    addItem: (id: number) => {
+      if (initialState.items.includes(id)) {
+        alert("Das Produkt ist bereits im Warenkorb!");
+      } else {
+        set((state) => ({
+          items: [...state.items, id],
+          count: state.count + 1,
+        }));
+      }
+    },
+    deleteItem: (id: number) => {
+      set((state) => ({
+        items: state.items.filter((item) => item !== id),
+        count: state.count - 1,
+      }));
+    },
+    clearCart: () => {
+      set({ items: [], count: 0 });
+    },
+  };
+});
+
+cartStore.subscribe((state) => {
+  localStorage.setItem(localStorageKey, JSON.stringify(state));
+});
