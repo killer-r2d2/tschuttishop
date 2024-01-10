94ccbbeb7cd529cc296ef114641c9ec849f40c98
Roger Killer <roger.killer@advery.ch>
2024-01-10 16:28:05 +0100

    remove unused hook useProducts from Products/[id]

--- app/Products/[id]/page.tsx
+++ app/Products/[id]/page.tsx
@@ -3,7 +3,6 @@ import useGetProductById from "@/hooks/useGetProductById";
 import ProductPage from "@/app/components/ProductPage";
 import { Container } from "@/app/components/Base/Container";
 import { SpinnerNext } from "@/app/components/Base/Spinner";
-import useProducts from "@/hooks/useProducts";
 
 export default function Page({ params }: { params: { id: number } }) {
   const productId: number = params.id;
