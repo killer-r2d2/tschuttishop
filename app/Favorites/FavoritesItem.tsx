30d3bd04807dbe622aaecda1d6aa6afa02f96b6b
Roger Killer <roger.killer@advery.ch>
2024-01-11 17:34:08 +0100

    style: add style from CartItem.tsx to FavoritesItem.tsx

--- app/Favorites/FavoritesItem.tsx
+++ app/Favorites/FavoritesItem.tsx
@@ -5,41 +5,44 @@ import { Button } from "@nextui-org/button";
 import { favoritesStore } from "@/store/favoritesStore";
 import { useStore } from "zustand";
 
-export default function FavoritesItem({ id, name, price }: Product) {
+export default function FavoritesItem({ id, name, price, size, category }: Product) {
   const removeFav = useStore(favoritesStore, (state) => state.removeFav);
   return (
-    <div className="grid grid-cols-6 gap-6 xl:gap-12 border-t pt-4 pb-4 pr-8 pl-8 items-center xl:hover:bg-slate-100 transition-colors">
-      <div className="p-2 col-span-full xl:col-span-1 relative min-h-[200px] xl:min-h-[130px]">
+    <div className="grid grid-cols-12 gap-y-12 xl:gap-x-12 border-t py-8">
+      <div className="col-span-full xl:col-span-6 relative">
         <Link href={`/Products/${id}`}>
           <Image
             src="/shirt-player.png"
-            fill
+            height={500}
+            width={500}
             objectFit="cover"
             alt={name}
-            className="rounded-xl h-[200px] max-w-full"
+            className="rounded-xl"
           />
         </Link>
       </div>
-      <div className="col-span-full xl:col-span-3">
+      <div className="col-span-full xl:col-span-6 flex flex-col gap-8">
         <Link href={`/Products/${id}`}>
-          <h2 className="text-xl">{name}</h2>
+          <h2 className="text-xl font-bold">{name}</h2>
         </Link>
         <div>
           <p>
             Preis: <span className="font-bold">{price} CHF</span>
           </p>
+          <p className="mt-2">Grösse: {size}</p>
+          <p className="mt-2">Kategorie: {category}</p>
         </div>
       </div>
-      <div className="col-span-full xl:col-span-2  flex w-full xl:justify-end">
-        <Button
-          size="sm"
-          variant="ghost"
-          color="danger"
-          onClick={() => removeFav(id)}
-        >
-          Entfernen
-        </Button>
-      </div>
+      <div>
+          <Button
+            size="sm"
+            variant="ghost"
+            color="danger"
+            onClick={() => removeFav(id)}
+          >
+            Entfernen
+          </Button>
+        </div>
     </div>
   );
 }
