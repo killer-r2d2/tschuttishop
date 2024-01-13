885eac47b06d1ec523d23c0a8cc7d44a723b398d
Roger Killer <roger.killer@advery.ch>
2024-01-13 15:27:13 +0100

    remove x padding from footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-slate-900 text-white p-5 shadow mt-5">
+    <footer className="bg-slate-900 text-white py-5">
       <Container>
         <div className="grid grid-cols-2 gap-4">
           <ul className="col-span-full md:col-span-1">
@@ -11,14 +11,10 @@ export function Footer() {
             <li>Bahnhofstrasse 1</li>
             <li>8000 Zürich</li>
             <li>
-              <Link
-                href="mailto:info@tschuttishop.ch
-"
-              >
+              <Link href="mailto:info@tschuttishop.ch">
                 info@tschuttishop.ch
               </Link>
             </li>
-            {/* phone */}
             <li>
               <Link href="tel:+41442211222">+41 44 221 12 22</Link>
             </li>
