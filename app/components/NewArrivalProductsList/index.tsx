eb8969f847098e64bf2568d4f49fdc3d604002c1
Roger Killer <roger.killer@advery.ch>
2024-01-13 11:35:59 +0100

    change text size of h2 in NewArrivalProductList

--- app/components/NewArrivalProductsList/index.tsx
+++ app/components/NewArrivalProductsList/index.tsx
@@ -26,7 +26,7 @@ export function NewArrivalProductsList() {
         <Container>
           <div className="flex items-center mb-5">
             <FireIcon className="h-14" />
-            <h2 className="text-5xl font-bold">Neu eingetroffen</h2>
+            <h2 className="text-3xl font-bold">Neu eingetroffen</h2>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
