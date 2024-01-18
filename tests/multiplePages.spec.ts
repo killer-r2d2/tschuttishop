5ef3fe48636edc4b7098cf70c7dcc5ae577eac4c
Roger Killer <roger.killer@advery.ch>
2024-01-18 20:39:51 +0100

    change multiplePages.spec.ts to check accessUrl and expectedUrl

--- tests/multiplePages.spec.ts
+++ tests/multiplePages.spec.ts
@@ -1,15 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('check the basic urls', async ({ page }) => {
+test('check the basic urls with expected redirections', async ({ page }) => {
   const urls = [
-    'http://localhost:3000/',
-    'http://localhost:3000/Login',
-    'http://localhost:3000/Favorites',
-    'http://localhost:3000/Cart',
-    'http://localhost:3000/AllProducts'
+    { 
+      accessUrl: 'http://localhost:3000/', 
+      expectedUrl: 'http://localhost:3000/' 
+    },
+    { 
+      accessUrl: 'http://localhost:3000/AllProducts', 
+      expectedUrl: 'http://localhost:3000/AllProducts' 
+    },
+    { 
+      accessUrl: 'http://localhost:3000/Favorites', 
+      expectedUrl: 'http://localhost:3000/Favorites' 
+    },
+    { 
+      accessUrl: 'http://localhost:3000/Cart', 
+      expectedUrl: 'http://localhost:3000/Login' 
+    },
   ];
-  for (const url of urls) {
-    await page.goto(url);
-    expect(page.url()).toBe(url);
+
+  for (const { accessUrl, expectedUrl } of urls) {
+    await page.goto(accessUrl);
+    expect(page.url()).toBe(expectedUrl);
   }
 });
